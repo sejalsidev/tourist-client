469360c348f859abdc56d35f2d1ecf8b09ef62b2
sejal dev SI <sejalsidev001@gmail.com>
2024-05-23 18:11:09 +0530

    register-otp-login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+// import Login from "./components/Login";
+import { Navbar } from "./components/Navbar";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Destination from "./components/Destination";
+import Tour from "./components/Tour";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Otp from "./components/Otp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="" style={{ marginTop: "68px" }}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="tours" element={<Tour />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="otp" element={<Otp />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
